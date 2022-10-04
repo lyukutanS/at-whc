e135543221ade7be224110cd2023ccbe1e9ddf98
lyukutan <lukytan@yandex.ru>
2022-10-04 22:40:31 +0300

    add springBoot and allure

---
+++ src/main/java/ru/softrust/automation/AutomationApplication.java
@@ -0,0 +1,13 @@
+package ru.softrust.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationApplication.class, args);
+    }
+}
+
